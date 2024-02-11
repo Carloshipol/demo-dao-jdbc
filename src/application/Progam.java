9bcd19dfd1b2c63dc02e7f1bc8de68f8e1946592
carloshipol <carlaogaruda30@gmail.com>
2024-02-11 18:35:28 -0300

    Department entity class

---
+++ src/application/Progam.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Department;
+
+public class Progam {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+		
+	}
+
+}
