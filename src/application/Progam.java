ac771752cedf6782beaeb12c24cce81b2b9265dc
carloshipol <carlaogaruda30@gmail.com>
2024-02-12 14:20:20 -0300

    Seller entity class

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,6 +1,9 @@
 package application;
 
-import entities.Department;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Progam {
 
@@ -8,7 +11,14 @@ public class Progam {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		
+		
+		System.out.println(seller);
+		
+		
+		
 		
 	}
 
