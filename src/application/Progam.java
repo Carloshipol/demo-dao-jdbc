648aa0c8a2e58665f22f637c5bd47e8495318a4e
carloshipol <carlaogaruda30@gmail.com>
2024-02-12 21:52:18 -0300

    Delete implementation

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,7 @@ import model.entities.Seller;
 public class Progam {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -47,6 +48,16 @@ public class Progam {
 		seller.setName("Camila  Conforte");
 		sellerDao.update(seller);
 		System.out.println("Update completed ");
+		
+		
+		System.out.println("\n=== TEST 6: seller delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed ");
+		
+		
+		sc.close();
 	}
 }
 
