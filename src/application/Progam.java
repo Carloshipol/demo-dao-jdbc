d52a04e541694c04ca77346785cd591be0d8a42a
carloshipol <carlaogaruda30@gmail.com>
2024-02-12 21:19:29 -0300

    Insert implementation

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,9 +30,19 @@ public class Progam {
 		list = sellerDao.findAll();
 		for ( Seller obj : list) {
 			System.out.println(obj);
+		
+		}
+		
+		
+
+		System.out.println("\n=== TEST 4: seller findByAll =====");
+		Seller newSeller = new Seller(null, "Carlos", "carlos@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 		}
 		
 		
 	}
 
-}
+
